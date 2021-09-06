69202fff4c049bb104377fdb0f44c7293013b949
Davi Otavio <85647007+daviotavio@users.noreply.github.com>
2021-09-06 10:16:22 -0300

    Add files via upload

---
+++ área de um triangulo/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace área_de_um_triangulo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float area,base1,altura;
+
+            Console.Write("A área de um triângulo é calculada utitlizando a fórmula: (área=base x altura)/2, portanto, determine a base: ");
+            base1 = float.Parse(Console.ReadLine());
+
+            Console.Write("Agora, determine a altura: ");
+            altura = float.Parse(Console.ReadLine());
+
+            area = (base1 * altura)/2;
+
+            Console.Write("A área do seu triângulo é: " + area);
+        }
+    }
+}
